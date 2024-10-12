b7aadf3f65fe778bfce02085838baaa81d982cbc
erisdll <erika.melloramos@gmail.com>
2024-10-12 20:16:42 -0300

    chore: code formatting

--- src/main/java/com/erika/eclipse_hotel/EclipseHotelApplication.java
+++ src/main/java/com/erika/eclipse_hotel/EclipseHotelApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EclipseHotelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EclipseHotelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EclipseHotelApplication.class, args);
+    }
 
 }
--- src/test/java/com/erika/eclipse_hotel/EclipseHotelApplicationTests.java
+++ src/test/java/com/erika/eclipse_hotel/EclipseHotelApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EclipseHotelApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
