ef4e70c5d3e34db2aa7165c8af2e960a4c82cb2a
erisdll <erika.melloramos@gmail.com>
2024-10-13 13:47:11 -0300

    chore: config update

--- src/main/java/com/erika/eclipse_hotel/exception/customer/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.erika.eclipse_hotel.exception.customer;
-
-import jakarta.persistence.EntityNotFoundException;
-
-public class CustomerNotFoundException extends EntityNotFoundException {
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/erika/eclipse_hotel/exception/room/RoomNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.erika.eclipse_hotel.exception.room;
-
-import jakarta.persistence.EntityNotFoundException;
-
-public class RoomNotFoundException extends EntityNotFoundException {
-    public RoomNotFoundException(String message) {
-        super(message);
-    }
-}
