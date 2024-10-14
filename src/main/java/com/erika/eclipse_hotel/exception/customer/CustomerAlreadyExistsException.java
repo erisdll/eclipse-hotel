16842fb77793593fd677a0b9d8bbba067ec5c345
erisdll <erika.melloramos@gmail.com>
2024-10-13 22:41:09 -0300

    fix: small bug fixes relating to exception handling

--- src/main/java/com/erika/eclipse_hotel/exception/customer/CustomerAlreadyExistsException.java
+++ src/main/java/com/erika/eclipse_hotel/exception/customer/CustomerAlreadyExistsException.java
@@ -3,7 +3,6 @@ package com.erika.eclipse_hotel.exception.customer;
 import org.springframework.dao.DataIntegrityViolationException;
 
 public class CustomerAlreadyExistsException extends DataIntegrityViolationException {
-
     public CustomerAlreadyExistsException(String message) {
         super(message);
     }
--- src/main/java/com/erika/eclipse_hotel/exception/reservation/ReservationCreationException.java
+++ src/main/java/com/erika/eclipse_hotel/exception/reservation/ReservationCreationException.java
@@ -1,7 +1,6 @@
 package com.erika.eclipse_hotel.exception.reservation;
 
 public class ReservationCreationException extends RuntimeException {
-
     public ReservationCreationException(String message) {
         super(message);
     }
